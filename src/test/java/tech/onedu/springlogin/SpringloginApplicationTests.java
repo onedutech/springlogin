6a3c0180372f585b422e30755488006872544f53
thejavator <elmehditahri@gmail.com>
2024-07-11 10:23:27 +0200

    initial commit

---
+++ src/test/java/tech/onedu/springlogin/SpringloginApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.onedu.springlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
